161c7ac81651eb07eb61f0ea213295ee48383216
emilyvera <evera5@illinois.edu>
2020-06-26 17:52:22 +0000

    Fix indent in js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -18,30 +18,30 @@ $(function() {
 
   function cb(start, end) {
     $('#reportrange span')
-        .html(
-            start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+      .html(
+        start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
   }
 
   $('#reportrange')
-      .daterangepicker(
-          {
-            startDate: start,
-            endDate: end,
-            ranges: {
-              'Today': [moment(), moment()],
-              'Yesterday':
-                  [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-              'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-              'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-              'This Month':
-                  [moment().startOf('month'), moment().endOf('month')],
-              'Last Month': [
-                moment().subtract(1, 'month').startOf('month'),
-                moment().subtract(1, 'month').endOf('month'),
-              ],
-            },
-          },
-          cb);
+    .daterangepicker(
+      {
+        startDate: start,
+        endDate: end,
+        ranges: {
+          'Today': [moment(), moment()],
+          'Yesterday':
+            [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month':
+            [moment().startOf('month'), moment().endOf('month')],
+          'Last Month': [
+            moment().subtract(1, 'month').startOf('month'),
+            moment().subtract(1, 'month').endOf('month'),
+          ],
+        },
+      },
 
+    cb);
   cb(start, end);
 });
