f2cb4b7af6a6871306b074e20b35683b6299929e
emilyvera <evera5@illinois.edu>
2020-06-29 21:32:56 +0000

    Switch to dynamic receipt display

--- src/main/java/servlets/SearchServlet.java
+++ src/main/java/servlets/SearchServlet.java
@@ -14,6 +14,7 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -23,12 +24,15 @@ import com.google.cloud.language.v1.Sentiment;
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import java.util.ArrayList;
+import java.util.HashSet; 
+import java.util.List;
+import java.util.Set; 
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-// import com.google.sps.servlets.Comment;
+import com.google.sps.data.Receipt;
 
 /** Servlet that adds a comment to the datastore. */
 @WebServlet("/search-receipts")
@@ -60,8 +64,41 @@ public class SearchServlet extends HttpServlet {
     //   comments.add(comment);
     // }
 
+    List<Receipt> receipts = new ArrayList<>();
+    addTestReceipts(receipts);
+
     Gson gson = new Gson();
     response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(label)); // make sure label went through
+    response.getWriter().println(gson.toJson(receipts)); 
+  }
+
+  private void addTestReceipts(List<Receipt> receipts) {
+    // Create test blobkey object
+    BlobKey blobkey = new BlobKey("Test");
+
+    // Create test categories tags
+    Set<String> categories = new HashSet<String>(); 
+    categories.add("Candy"); 
+    categories.add("Drink"); 
+    categories.add("Personal");
+
+    // Create test text translation
+    String rawText = "Wal-Mart\nAlways Low Prices at Wal-Mart\nAlways:\nWe Sell For Less\nManager Tim Stryczek...";
+    
+    // Add test receipts
+    Receipt receipt = new Receipt(1, 123, 6292020, blobkey, "img/walmart-receipt.jpg", 26.12, "Walmart", "hello", categories, rawText);
+    receipts.add(receipt);
+
+    Receipt receipt1 = new Receipt(2, 123, 6302020, blobkey, "img/walmart-receipt.jpg", 26.12, "Walmart", "hello", categories, rawText);
+    receipts.add(receipt1);
+
+    Receipt receipt2 = new Receipt(3, 123, 7012020, blobkey, "img/walmart-receipt.jpg", 26.12, "Walmart", "test", categories, rawText);
+    receipts.add(receipt2);
+
+    Receipt receipt3 = new Receipt(4, 123, 7022020, blobkey, "img/walmart-receipt.jpg", 26.12, "Walmart", "test", categories, rawText);
+    receipts.add(receipt3);
+
+    Receipt receipt4 = new Receipt(5, 123, 7032020, blobkey, "img/walmart-receipt.jpg", 26.12, "Walmart", "test", categories, rawText);
+    receipts.add(receipt4);
   }
 }
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -12,20 +12,58 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/** Fetches comments from the server and adds them to the DOM. */
+/** Fetch receipts from the server and add them to the DOM. */
 async function searchReceipts() {
   const label = document.getElementById("search-input").value;
   const response = await fetch(`/search-receipts?label=${label}`);
   const receipts = await response.json();
-//   console.log(receipts);
-//   const commentsDisplayed = document.getElementById("comment-list");
-//   commentsDisplayed.innerHTML = "";
 
-//   comments.forEach((comment) => {
-//     commentsDisplayed.appendChild(createCommentElement(comment));
-//   });
+  // Clear out old receipt display and populate them with new queried receipts.
+  const receiptsDisplayed = document.getElementById("receipt-list");
+  receiptsDisplayed.innerHTML = '';
+  
+  receipts.forEach((receipt) => {
+    receiptsDisplayed.innerHTML += createReceiptCardElement(receipt);
+  });
 }
 
+/** Returns an HTML string that represents a receipt card.
+  * This card displays trasaction date, store name, trasaction total, categories, 
+  * receipt photo, and view/edit/delete buttons. */
+function createReceiptCardElement(receipt) {
+  let categories = Array.from(receipt.categories)
+  var formattedHTML = '<div class="col-md-6">'+
+'          <div class="card mb-4 box-shadow">'+
+'            <p class="card-text align-self-end">' + receipt.timestamp + '</p>'+
+'            <p class="card-text">' + receipt.store + '</p>'+
+'            <p class="card-text">Total: $' + receipt.price + '</p>'+
+'            <div class="row">'+
+'              <div class="col-4 d-flex justify-content-center">'+
+'                <h4><span class="badge badge-pill badge-secondary">' + categories[0] + '</span></h4>'+
+'              </div>'+
+'              <div class="col-4 d-flex justify-content-center">'+
+'                <h4><span class="badge badge-pill badge-secondary">' + categories[1] + '</span></h4>'+
+'              </div>'+
+'              <div class="col-4 d-flex justify-content-center">'+
+'                <h4><span class="badge badge-pill badge-secondary">' + categories[2] + '</span></h4>'+
+'              </div>'+
+'            </div>'+
+'            <img src=' + receipt.imageURL + ' alt="Receipt image" class="img-fluid receipt-img" />'+
+'            <div class="card-body">'+
+'              <div class="d-flex justify-content-between align-items-center">'+
+'                <div class="btn-group">'+
+'                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button'+
+'                  ><button type="button" class="btn btn-sm btn-outline-secondary">Edit</button'+
+'                  ><button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>'+
+'                </div>'+
+'              </div>'+
+'            </div>'+
+'          </div>'+
+'        </div>';
+	return formattedHTML;
+}
+
+/* Calendar handler. */
 $(function() {
   const start = moment().subtract(29, 'days');
   const end = moment();
