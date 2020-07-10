c0ddfec3c4666a2240303789046f7b06e65a220b
Ava Garcia <avagarcia@google.com>
2020-07-10 15:32:52 +0000

    Mock builders and their results

--- src/test/java/ReceiptAnalysisTest.java
+++ src/test/java/ReceiptAnalysisTest.java
@@ -18,6 +18,9 @@ import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
 import com.google.protobuf.ByteString;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -26,15 +29,25 @@ import java.net.URL;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ByteString.class, URL.class})
+@PrepareForTest(
+    {AnnotateImageRequest.class, ByteString.class, Feature.class, Image.class, URL.class})
 public final class ReceiptAnalysisTest {
   private static final ByteString IMAGE_BYTES = ByteString.copyFromUtf8("byte string");
+
+  @Mock private AnnotateImageRequest request;
+  @Mock private Feature feature;
+  @Mock private Image image;
+
+  @Mock(answer = Answers.RETURNS_SELF) private AnnotateImageRequest.Builder requestBuilder;
+  @Mock(answer = Answers.RETURNS_SELF) private Feature.Builder featureBuilder;
+  @Mock(answer = Answers.RETURNS_SELF) private Image.Builder imageBuilder;
 
   @Before
   public void setUp() {
@@ -49,5 +62,17 @@ public final class ReceiptAnalysisTest {
 
     mockStatic(ByteString.class);
     when(ByteString.readFrom(inputStream)).thenReturn(IMAGE_BYTES);
+
+    when(imageBuilder.build()).thenReturn(image);
+    mockStatic(Image.class);
+    when(Image.newBuilder()).thenReturn(imageBuilder);
+
+    when(featureBuilder.build()).thenReturn(feature);
+    mockStatic(Feature.class);
+    when(Feature.newBuilder()).thenReturn(featureBuilder);
+
+    when(requestBuilder.build()).thenReturn(request);
+    mockStatic(AnnotateImageRequest.class);
+    when(AnnotateImageRequest.newBuilder()).thenReturn(requestBuilder);
   }
 }
