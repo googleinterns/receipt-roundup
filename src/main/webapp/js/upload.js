594514ab9f7754e48f97acab7c5232b3bd0425f5
Basaam Malik <basaammalik@gmail.com>
2020-07-27 21:47:47 +0000

    Disable file input while processing upload.

--- src/main/webapp/js/upload.js
+++ src/main/webapp/js/upload.js
@@ -53,7 +53,8 @@ async function uploadReceipt(event) {
   // Restore the cursor after the upload request has loaded.
   document.body.style.cursor = 'default';
 
-  // Create an alert and re-enable the submit button if there is an error.
+  // Create an alert and re-enable the submit button and file input if there is
+  // an error.
   if (response.status !== 200) {
     const submitButton = document.getElementById('submit-receipt');
     submitButton.innerText = 'Error!';
@@ -64,7 +65,8 @@ async function uploadReceipt(event) {
 
     alert(await response.text());
 
-    // Restore the submit button.
+    // Restore the file input and submit button.
+    fileInput.disabled = false;
     submitButton.disabled = false;
     submitButton.innerText = 'Add Receipt';
     return;
@@ -93,7 +95,7 @@ async function fetchBlobstoreUrl() {
 }
 
 /**
- * Displays the loading animation and disables the submit button.
+ * Displays the loading animation and disables the submit button and file input.
  * @return {number} The ID value of the setInterval() timer.
  */
 function startLoading() {
@@ -102,6 +104,9 @@ function startLoading() {
   const submitButton = document.getElementById('submit-receipt');
   submitButton.disabled = true;
   submitButton.innerText = 'Analyzing...';
+
+  const fileInput = document.getElementById('receipt-image-input');
+  fileInput.disabled = true;
 
   // Display the loading image, which is hidden by default.
   const loadingBar = document.getElementsByClassName('ldBar')[0].ldBar;
