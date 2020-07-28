5d42916a237b6dcffda63b3227db23e09f6fff36
emilyvera <evera5@illinois.edu>
2020-07-28 05:01:36 +0000

    Refactor some statements to be guard clauses

--- src/main/java/data/SpendingAnalytics.java
+++ src/main/java/data/SpendingAnalytics.java
@@ -44,19 +44,27 @@ public class SpendingAnalytics {
     Double price = (Double) receipt.getProperty("price");
 
     // Don't add to hashmap if either store or price are invalid.
-    if (!Strings.isNullOrEmpty(store) && price != null) {
-      // Either update the existing entry or add a new one if it doesn't exist.
-      if (storeAnalytics.containsKey(store)) {
-        storeAnalytics.put(store, storeAnalytics.get(store) + price);
-      } else {
-        storeAnalytics.put(store, price);
-      }
+    if (Strings.isNullOrEmpty(store) || price == null) {
+      return;
+    }
+
+    // Either update the existing entry or add a new one if it doesn't exist.
+    if (storeAnalytics.containsKey(store)) {
+      storeAnalytics.put(store, storeAnalytics.get(store) + price);
+    } else {
+      storeAnalytics.put(store, price);
     }
   }
 
   /** Updates categoryAnalytics hashmap with category info from the passed in receipt. */
   private void updateCategoryAnalytics(Entity receipt, HashMap<String, Double> categoryAnalytics) {
     Double price = (Double) receipt.getProperty("price");
+
+    // Don't add any categories to hashmap if price is invalid.
+    if (price == null) {
+      return;
+    }
+
     ImmutableSet<String> categories;
 
     // If categories is null for a particular receipt, just skip it and continue.
@@ -67,14 +75,11 @@ public class SpendingAnalytics {
     }
 
     for (String category : categories) {
-      // Don't add to hashmap if price is invalid.
-      if (price != null) {
-        // Either update the existing entry or add a new one if it doesn't exist.
-        if (categoryAnalytics.containsKey(category)) {
-          categoryAnalytics.put(category, categoryAnalytics.get(category) + price);
-        } else {
-          categoryAnalytics.put(category, price);
-        }
+      // Either update the existing entry or add a new one if it doesn't exist.
+      if (categoryAnalytics.containsKey(category)) {
+        categoryAnalytics.put(category, categoryAnalytics.get(category) + price);
+      } else {
+        categoryAnalytics.put(category, price);
       }
     }
   }
