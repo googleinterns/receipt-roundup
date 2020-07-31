d6fadbce2ae1691936a6808edce5c4c611a2ef2c
emilyvera <evera5@illinois.edu>
2020-07-31 22:00:03 +0000

    Remove unncessary comment

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -89,7 +89,7 @@ async function searchReceipts(params) {
   displayReceipts(receipts);
 }
 
-/** Scrolls to the top of the page when called. */
+/** Scrolls to the top of the page. */
 function scrollToTop() {
   document.body.scrollTop = 0;             // For Safari
   document.documentElement.scrollTop = 0;  // For Chrome, Firefox, IE and Opera
